610e043b1d723e953e5c52c778a38d6a3eb932c4
TodorTodorin <toti.1@abv.bg>
2015-02-16 10:50:22 +0200

    Added some content

--- public/script.js
+++ public/script.js
@@ -1,4 +1,17 @@
-/**
- * Created by Todor Todorin on 2/16/2015.
- */
-alert('shit');
+document.getElementById('add').onclick = function() {
+    var a = document.getElementById('a').valueAsNumber;
+    var b = document.getElementById('b').valueAsNumber;
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('POST', '/add');
+
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.responseType = 'json';
+
+    xhr.onload = function() {
+        document.getElementById('result').innerHTML = xhr.response;
+    };
+
+    xhr.send(JSON.stringify({ a: a, b: b }));
+};
